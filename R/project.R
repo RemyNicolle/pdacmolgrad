97e0a898e0fb58f2be4cba7dd00d653f554bdf95
remy.nicolle <remy.nicolle@ligue-cancer.net>
2020-02-03 10:59:18 +0100

    Recreating package

--- R/project.R
+++ R/project.R
@@ -1,18 +1,3 @@
-# Hello, world!
-#
-# This is an example function named 'hello'
-# which prints 'Hello, world!'.
-#
-# You can learn more about package authoring with RStudio at:
-#
-#   http://r-pkgs.had.co.nz/
-#
-# Some useful keyboard shortcuts for package authoring:
-#
-#   Build and Reload Package:  'Cmd + Shift + B'
-#   Check Package:             'Cmd + Shift + E'
-#   Test Package:              'Cmd + Shift + T'
-
 
 .getUGM=function (m, g, w)
 {
@@ -30,25 +15,25 @@
 
 
 
-#' projectMolGrade
+#' projectMolGrad
 #'
 #' @param newexp gene expression matrix or dataframe with gene in row and sample in columns.
 #' @param geneSymbols vector of gene symbols for the newexp dataset (simply set to rownames if the nexzexp is already in single values per gene symbols)
 #'
-#' @return data frame of three oroejctions
+#' @return data frame of three projections
 #' @export
 #'
 #' @examples
-projectMolGrade=function(newexp,geneSymbols){
+projectMolGrad=function(newexp,geneSymbols){
 
   if(nrow(newexp)!= length(geneSymbols)){
     stop("geneSymbols should be a vector of gene symbols exactly corresponding to each row of the newexp dataset")
   }
-  expg=.getUGM(newexp,geneSymbols,rowSds(as.matrix(newexp)))
+  expg=pdacmolgrad:::.getUGM(newexp,geneSymbols,rowSds(as.matrix(newexp)))
 
-  data(molgradeSystems)
 
-  comg=intersect(rownames(molgradeSystems$pdx),rownames(expg))
+
+  comg=intersect(rownames(pdacmolgrad:::molgradeSystems$pdx),rownames(expg))
   if(length(comg)<5000){
     pdxproj=NA
   }else{
@@ -58,22 +43,22 @@ projectMolGrade=function(newexp,geneSymbols){
     pdxproj=(pdxprojInvSc-mean(pdxprojInvSc))/(3*sd(pdxprojInvSc))
   }
 
-  comg=intersect(rownames(molgradeSystems$icgc),rownames(expg))
+  comg=intersect(rownames(pdacmolgrad:::molgradeSystems$icgc),rownames(expg))
   scexp=scale(expg[comg,])
   if(length(comg)<5000){
     icgcproj=NA
   }else{
-    invs=MASS::ginv(as.matrix(molgradeSystems$icgc[comg,]))
+    invs=MASS::ginv(as.matrix(pdacmolgrad:::molgradeSystems$icgc[comg,]))
     icgcprojInvSc=t(t((t(	scexp )%*% t(invs))))[,1]
     icgcproj=(icgcprojInvSc-mean(icgcprojInvSc))/(3*sd(icgcprojInvSc))
 
   }
-  comg=intersect(rownames(molgradeSystems$puleo),rownames(expg))
+  comg=intersect(rownames(pdacmolgrad:::molgradeSystems$puleo),rownames(expg))
   scexp=scale(expg[comg,])
   if(length(comg)<5000){
     puleoproj=NA
   }else{
-    invs=MASS::ginv(as.matrix(molgradeSystems$puleo[comg,]))
+    invs=MASS::ginv(as.matrix(pdacmolgrad:::molgradeSystems$puleo[comg,]))
     puleoprojInvSc=t(t((t(	scexp )%*% t(invs))))[,2]
     puleoproj=(puleoprojInvSc-mean(puleoprojInvSc))/(3*sd(puleoprojInvSc))
   }
